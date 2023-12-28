f47a545bf6ac4d287e86fe01230765c204a7c797
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-12-28 08:41:35 -0300

    chore: add frontend nextjs initial config

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'TranscriptionAI',
+  description:
+    'A basic transcriber to define video titles and descriptions using artificial intelligence',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
