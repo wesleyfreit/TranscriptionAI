cfe37b0783b78e2abcf8eebd575f714dc7a691bb
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-12-28 14:11:06 -0300

    feat: add fastify and set hello router and an routes.http file

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+import 'dotenv/config';
+import { fastify } from 'fastify';
+
+const app = fastify();
+const port = (process.env.PORT as unknown as number) || 3333;
+
+app.get('/', () => {
+  return 'Hello World!';
+});
+
+app.listen({ port: port }, () => {
+  console.log(`listening on http://localhost:${port} `);
+});
